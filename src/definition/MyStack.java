59b8ff07e5c2c3682a9d1b3c7a08159bbcb94c71
Ananya015 <jainananya78@gmail.com>
2020-03-21 19:08:56 +0530

    override method of AdtStack interface in MyStack class

--- src/definition/MyStack.java
+++ src/definition/MyStack.java
@@ -1,4 +1,25 @@
 package definition;
 
-public class MyStack<E> {
+import adt.AdtStack;
+
+public class MyStack<E> implements AdtStack<E> {
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(E item) {
+
+    }
 }
